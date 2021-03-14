bf0a81380117f5f5c59fe5a79a2d6ba30579de49
joe <joelibral2693@gmail.com>
2021-03-14 20:07:58 +0330

    create list,detail,update,delete and make serializers.py and urls this

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,14 @@
 from django.urls import path
-
+from .views import (
+    PostAPIView, PostDetail, PostDestroy,
+    PostUpdate, PostCreate, 
+)
 app_name = 'api'
 
 urlpatterns = [
-    
+    path('post-list/',PostAPIView.as_view(),name='post-list'),
+    path('post-detail/<slug>/', PostDetail.as_view(), name='post-detail'),
+    path('post-update/<int:pk>/', PostUpdate.as_view(),name='post-update'),
+    path('post-destroy/<abc>/', PostDestroy.as_view(), name='post-destroy'),
+    path('post-create/', PostCreate.as_view(), name='post-create'),
 ]
--- api/views.py
+++ api/views.py
@@ -1,3 +1,43 @@
 from django.shortcuts import render
 
 # Create your views here.
+from posts.models import Post
+from rest_framework.generics import (
+    ListAPIView, RetrieveAPIView, DestroyAPIView,
+    UpdateAPIView,CreateAPIView
+)
+from .serializers import (
+    PostListSerializer, PostDetailSerializer, PostCreateSerializer,
+    
+)
+
+class PostAPIView(ListAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostListSerializer
+
+
+class PostDetail(RetrieveAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostDetailSerializer
+    lookup_field = 'slug' #for change default use pk
+    #lookup_url_kwarg = 'abc' >>> for change input url --> path('<abc>/', ... ),
+
+class PostDestroy(DestroyAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostDetailSerializer
+    lookup_field = 'slug' #for change default use pk
+    lookup_url_kwarg = 'abc'
+
+class PostUpdate(UpdateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostDetailSerializer
+
+    def perform_update(self, serializer):
+        serializer.save(user=self.request.user)
+
+class PostCreate(CreateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostCreateSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
